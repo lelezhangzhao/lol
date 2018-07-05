426211bcbc8e684f9369fd2d65d0f62644a7b48e
lelezhangzhao <lelezhangzhao@163.com>
2018-07-05 17:04:12 +0800

    modify other#上传css和js目录

---
+++ thinkphp_5.0.20_with_ext/public/static/js/action.js
@@ -0,0 +1,38 @@
+
+$(function(){
+
+    $("li#saishiliebiao").click(function(){
+        
+    });
+    $("li#xiazhujilu").click(function(){
+        alert("xiazhujilu");
+    });
+    $("li#chongzhi").click(function(){
+        alert("chongzhi");
+    });
+    $("li#tixian").click(function(){
+        alert("tixian");
+    });
+    $("li#zhuanzhang").click(function(){
+        alert("zhuanzhang");
+    });
+    $("li#zhuanzhangjilu").click(function(){
+        alert("zhuanzhangjilu");
+    });
+    $("li#xianshigerenxinxi").click(function(){
+        alert("xianshigerenxinxi");
+    });
+    $("li#xiugaimima").click(function(){
+        alert("xiugaimima");
+    });
+    $("li#xiugaierjimima").click(function(){
+        alert("xiugaierjimima");
+    });
+
+    $("li#xitonggonggao").click(function(){
+        alert("xitonggonggao");
+    });
+
+
+
+});
